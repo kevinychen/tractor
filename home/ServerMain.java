2c8fbe61e7eff1fc30ac1ea13cd894404374a195
kevinychen <binonunquineist@gmail.com>
2013-06-05 10:40:20 -0500

    Add a Server main file.

---
+++ home/ServerMain.java
@@ -0,0 +1,21 @@
+package home;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import server.Server;
+import view.NullView;
+
+public class ServerMain
+{
+    public static void main(String... args) throws Exception
+    {
+        /* Find external IP */
+        String IP = new BufferedReader(new InputStreamReader(new URL(
+                "http://api.exip.org/?call=ip").openStream())).readLine();
+        System.out.println("Your IP is " + IP + ".");
+
+        new Server(new NullView("[server]")).startServer(3003);
+    }
+}
