94ba91063a921afdfa75341c4e7a1f1586573aed
kevinychen <binonunquineist@gmail.com>
2013-06-16 15:51:46 -0400

    Remove auto-generated method stubs.

--- view/NullView.java
+++ view/NullView.java
@@ -16,141 +16,100 @@ public class NullView extends View
     @Override
     public void start()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void createRoom()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void closeRoom()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void joinRoom()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void leaveRoom()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void requestStartGame()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void startGame(Game game)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void requestStartRound()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void startRound()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void requestFriendCards(int numFriends)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void notifyCanMakeKitty(int kittySize)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void drawCard(Card card, int playerID)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void showCards(Play play)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void selectFriendCards(FriendCards friendCards)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void makeKitty(Play play)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void playCards(Play play)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void finishTrick(Trick trick, int winnerID)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void endRound()
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void notify(String notification)
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void repaint()
     {
-        // TODO Auto-generated method stub
-
     }
-
 }
